499a11540c7176d0908c3d9f266933b5d2761781
Victor Oliveira Santos <victor_oliveirasantos@hotmail.com>
2021-09-27 18:09:14 -0300

    :pencil: add elements selector

---
+++ elements-select/script.js
@@ -0,0 +1,34 @@
+// getElementById (element)
+// getElementsByClassName (HTMLCollection)
+// getElementsByTagName (HTMLCollection)
+// querySelector (element)
+// querySelectorAll (NodeList)
+
+
+
+// getElementById()
+
+// const element = document.getElementById('studies-title')
+// console.log(element)
+
+// getElementsByClassName()
+
+// const elementByClassName = document.getElementsByClassName('one')
+// console.log(elementByClassName)
+
+// getElementsByTagName()
+
+// const element = document.getElementsByTagName('h1')
+// console.log(element)
+
+// querySelector()
+
+// const element = document.querySelector('[src]')    
+// console.log(element)
+
+// querySelectorAll
+
+// const elements = document.querySelectorAll('.one')
+// console.log(elements)
+
+// elements.forEach(el => console.log(el))
